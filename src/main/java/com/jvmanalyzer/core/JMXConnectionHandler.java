4b53b1b0b0a8a79489fd274227bbc25373345d81
musib <musib.somnath@gmail.com>
2017-03-12 04:55:54 +0530

    Moved to maven project structure

--- src/main/java/com/jvmanalyzer/core/JMXConnectionHandler.java
+++ src/main/java/com/jvmanalyzer/core/JMXConnectionHandler.java
@@ -1,29 +1,25 @@
 package com.jvmanalyzer.core;
 
-public class ProcessIdentifierConnectionHandler implements ConnectionHandler {
+public class JMXConnectionHandler implements ConnectionHandler{
 
-	@Override
 	public void connect() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
-	@Override
 	public void disconnect() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
-	@Override
 	public void reconnect() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
-	@Override
 	public boolean isConnection() {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
+	
 }
--- src/main/java/com/jvmanalyzer/core/ProcessIdentifierConnectionHandler.java
+++ src/main/java/com/jvmanalyzer/core/ProcessIdentifierConnectionHandler.java
@@ -1,29 +1,25 @@
 package com.jvmanalyzer.core;
 
-public class JMXConnectionHandler implements ConnectionHandler{
+public class ProcessIdentifierConnectionHandler implements ConnectionHandler {
 
-	@Override
 	public void connect() {
-		
+		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void disconnect() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void reconnect() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public boolean isConnection() {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
+	
 }
